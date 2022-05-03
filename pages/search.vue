06b73ccef7d914845925cc1461d5c09a3e7311a4
NicolaSpadari <spadari.nicola@gmail.com>
2022-05-03 11:28:31 +0200

    feat: auto search with debounce composable

--- pages/search.vue
+++ pages/search.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="container">
         <p>Search user:</p>
-        <input v-model="kw" type="text" class="border-1 border-dark-800" placeholder="search" @keyup.enter="search()">
-        <button class="border-1 border-dark-800" @click="search()">
-            search
-        </button>
+        <input v-model="kw" type="text" class="border-1 border-dark-800" placeholder="search">
 
         <div class="mt-7">
             <ClientOnly>
@@ -17,20 +14,18 @@
             </ClientOnly>
         </div>
 
-        <div v-if="history.length">
-            <ClientOnly>
-                <p>Your search history:</p>
-                <NuxtLink v-for="user in history" :key="`history-${user.id}`" :to="`/services/${user.id}`" class="mt-10">
-                    <div class="flex space-x-2">
-                        <UserAvatar :src="user.picture" size="small" />
-                        <p>{{ user.name }} ({{ user.userName }})</p>
-                        <button class="border-1 border-dark-800 w-5 h-5" @click.prevent="removeFromHistory(user)">
-                            X
-                        </button>
-                    </div>
-                </NuxtLink>
-            </ClientOnly>
-        </div>
+        <ClientOnly>
+            <p>Your search history:</p>
+            <NuxtLink v-for="user in history" :key="`history-${user.id}`" :to="`/services/${user.id}`" class="mt-10">
+                <div class="flex space-x-2">
+                    <UserAvatar :src="user.picture" size="small" />
+                    <p>{{ user.name }} ({{ user.userName }})</p>
+                    <button class="border-1 border-dark-800 w-5 h-5" @click.prevent="removeFromHistory(user)">
+                        X
+                    </button>
+                </div>
+            </NuxtLink>
+        </ClientOnly>
     </div>
 </template>
 
@@ -39,15 +34,17 @@
         middleware: ["auth"]
     });
 
-    const kw = ref("");
+    const kw = useDebouncedRef("", 300);
     const results = ref<FullUser[]>([]);
     const client = useSupabaseClient();
     const me = useSupabaseUser();
+    const loading = ref(false);
     const { history, addToHistory, removeFromHistory } = useHistory();
 
-    const search = async() => {
-        const { data } = await useAsyncData(`search-${kw.value}`, async(): Promise<FullUser[]> => {
-            const { data, error } = await client.from("users").select("id, name, picture, email").ilike("email", `%${kw.value}%`).limit(10);
+    const search = async (term: string) => {
+        console.log(`searching for ${term}`);
+        const { data, pending } = await useAsyncData(`search-${term}`, async (): Promise<FullUser[]> => {
+            const { data, error } = await client.from("users").select("id, name, picture, email").ilike("email", `%${term}%`).limit(10);
 
             if (error) {
                 console.error("error searching user: ", error);
@@ -70,7 +67,16 @@
             }
         });
 
+        loading.value = pending.value;
         results.value = [];
         results.value.push(...data.value);
     };
+
+    watch(kw, (newVal) => {
+        if (newVal !== "") {
+            search(newVal);
+        } else {
+            results.value = [];
+        }
+    });
 </script>
